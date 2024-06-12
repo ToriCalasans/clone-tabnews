255a1b10f599e66980b837084d9d676dcab27ae7
Tori Calasans <56488155+ToriCalasans@users.noreply.github.com>
2024-06-12 18:53:22 +0000

    Modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mirer!To evoluindo, se concorda da uma risadinha!</h1>
+    return <h1>Olá Mirer!To evoluindo, se concorda da uma risadinha! </h1>
     
 }
 export default Home;
