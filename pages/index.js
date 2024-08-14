9f94a4ac0141bf442ea57cc3e9ddb0633f5922c1
ToriCalasans <calasanstori@gmail.com>
2024-08-14 12:24:49 -0300

    Adiciona  scripts e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Olá Mirer!To evoluindo, se concorda da uma risadinha! </h1>
-    
+  return <h1>Olá Mirer!To evoluindo, se concorda da uma risadinha! </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+function teste() {
+  console.log("indentação errada");
 }
 export default Home;
