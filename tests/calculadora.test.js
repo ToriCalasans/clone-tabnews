c2e1c00c87dd27e9b086260af9552b6520ec19e6
Tori Calasans <56488155+ToriCalasans@users.noreply.github.com>
2025-07-08 19:03:38 +0000

    feito testes de teste

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,15 +1,13 @@
 test("nome do teste", callbackFunction);
-function callbackFunction(){
-}
+function callbackFunction() {}
 
-test("nome do teste2", function(){
+test("nome do teste2", function () {
   console.log("Simplificando o teste");
 });
 
-test("nome do teste3", ()=>{
+test("nome do teste3", () => {
   console.log("utilizando arrow function para simplificar ainda mais");
 });
-test("espero que 1 seja 1",()=>{
-  expected(1).toBE(1);
+test("espero que 1 seja 1", () => {
+  expect(9).toBe(9);
 });
-
