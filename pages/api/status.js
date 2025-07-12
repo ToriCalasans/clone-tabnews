28b00f9802ecdccf66229efbc902ae85ee9827b1
ToriCalasans <calasanstori@gmail.com>
2025-07-12 16:49:53 -0300

    iniciado os estudos com api

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,3 +1,4 @@
 function status(request, response) {
-  response.status(200).send("Enviar");
+  response.status(200).json({ chave: "Enviar" });
 }
+export default status;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 function Home() {
-  return;
   <div>
     <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-  </div>;
+  </div>
+  return;
 }
+
 export default Home;
