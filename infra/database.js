cb6cb9dc23e53e97de22dffb555da3ae076b179d
Tori Calasans <56488155+ToriCalasans@users.noreply.github.com>
2025-09-05 12:26:48 +0000

    feito

--- infra/database.js
+++ infra/database.js
@@ -3,10 +3,11 @@ async function query(queryObject) {
   const client = new Client({
     host: "localhost",
     port: 5432,
-    user: "postgres",
-    dartabase: "postgres",
-    password: "local_password",
+    user: 'postgres',
+    dartabase: 'postgres',
+    password: 'local_password',
   });
+  await client.connect();
   const result = await client.connect(queryObject);
   await client.end();
   return result;
