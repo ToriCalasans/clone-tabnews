6141e7a7ead46fda148b4bfd2adee264314479a4
Tori Calasans <56488155+ToriCalasans@users.noreply.github.com>
2025-09-24 19:00:46 +0000

    feito alteração no jest

--- infra/database.js
+++ infra/database.js
@@ -3,12 +3,12 @@ async function query(queryObject) {
   const client = new Client({
     host: "localhost",
     port: 5432,
-    user: 'postgres',
-    dartabase: 'postgres',
-    password: 'local_password',
+    user: "postgres",
+    dartabase: "postgres",
+    password: "local_password",
   });
   await client.connect();
-  const result = await client.connect(queryObject);
+  const result = await client.query(queryObject);
   await client.end();
   return result;
 }
