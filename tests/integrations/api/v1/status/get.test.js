f52c7d6b6d58957b551d1c5ce9d35671dd84110b
ToriCalasans <calasanstori@gmail.com>
2025-10-13 21:37:58 -0300

    feito alterações nos testes e no banco local

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
-test("GET to /api/v1/status should return 200", async() => {
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
